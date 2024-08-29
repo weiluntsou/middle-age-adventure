e847fc695ae843008dc342691b85be486bf649e0
weiluntsou <tzou0605@gmail.com>
2024-08-30 00:08:46 +0800

    0829

--- my_2d_game/src/main/GamePanel.java
+++ my_2d_game/src/main/GamePanel.java
@@ -48,29 +48,54 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run(){
         double drawInterval = 1000000000 / FPS;
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        // double nextDrawTime = System.nanoTime() + drawInterval;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
         while (gameThread != null){
-            
-            update();
 
-            
+            currentTime = System.nanoTime();
 
-            repaint();
-            
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime / 1000000;
-                if (remainingTime < 0){
-                    remainingTime = 0;
-                }
+            delta += (currentTime - lastTime ) / drawInterval;
+            timer += (currentTime - lastTime );
+            lastTime = currentTime;
 
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval; 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
             }
+            if (timer >= 1000000000){
+                System.out.println("FPS:"+drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+
+            // update();
+
+            
+
+            // repaint();
+            
+            // try {
+            //     double remainingTime = nextDrawTime - System.nanoTime();
+            //     remainingTime = remainingTime / 1000000;
+            //     if (remainingTime < 0){
+            //         remainingTime = 0;
+            //     }
+
+            //     Thread.sleep((long) remainingTime);
+
+            //     nextDrawTime += drawInterval; 
+            //     drawCount ++;
+            // } catch (InterruptedException e) {
+            //     e.printStackTrace();
+            // }
             
         }
 
